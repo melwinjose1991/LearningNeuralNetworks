06eda969732993d2a2729aaa5b33ea9630aa6aec
Melwin Jose <melwin.jose.1991@gmail.com>
2017-09-16 14:25:33 -0400

    LearningNNs :: 4@ Welch Labs

--- Tutorials/Welch Labs/NN.py
+++ Tutorials/Welch Labs/NN.py
@@ -1,5 +1,9 @@
+#%% Imports
 import numpy as np
 
+
+
+#%% Data
 # X = (hours sleeping, hours studying), y = Score on test
 X = np.array(([3,5], [5,1], [10,2]), dtype=float)
 y = np.array(([75], [82], [93]), dtype=float)
@@ -9,6 +13,8 @@ X = X/np.amax(X, axis=0)
 y = y/100 #Max test score is 100
 
 
+
+#%% Neural Network Class
 class Neural_Network(object):
     def __init__(self):        
         #Define Hyperparameters
@@ -17,11 +23,11 @@ class Neural_Network(object):
         self.hiddenLayerSize = 3
         
         #Weights (parameters)
-        self.W1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
-        self.W2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
+        self.W1 = np.random.randn(self.inputLayerSize,self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize,self.outputLayerSize)
         
     def forward(self, X):
-        #Propagate inputs though network
+        #Propogate inputs though network
         self.z2 = np.dot(X, self.W1)
         self.a2 = self.sigmoid(self.z2)
         self.z3 = np.dot(self.a2, self.W2)
@@ -32,3 +38,39 @@ class Neural_Network(object):
         #Apply sigmoid activation function to scalar, vector, or matrix
         return 1/(1+np.exp(-z))
     
+    def sigmoidPrime(self,z):
+        #Gradient of sigmoid
+        return np.exp(-z)/((1+np.exp(-z))**2)
+    
+    def costFunction(self, X, y):
+        #Compute cost for given X,y, use weights already stored in class.
+        self.yHat = self.forward(X)
+        J = 0.5*sum((y-self.yHat)**2)
+        return J
+        
+    def costFunctionPrime(self, X, y):
+        #Compute derivative with respect to W and W2 for a given X and y:
+        self.yHat = self.forward(X)
+        
+        delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
+        dJdW2 = np.dot(self.a2.T, delta3)
+        
+        delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
+        dJdW1 = np.dot(X.T, delta2)  
+        
+        return dJdW1, dJdW2
+
+
+
+#%% Training
+scalar = 1
+NN = Neural_Network()
+
+for i in range(1,10):
+    cost = NN.costFunction(X,y)
+    dJdW1, dJdW2 = NN.costFunctionPrime(X,y)
+    # print(cost, dJdW1, dJdW2)
+    print(cost)
+    
+    NN.W1 = NN.W1 - scalar*dJdW1
+    NN.W2 = NN.W2 - scalar*dJdW2
